3f95f5584e3678f78a9bcf99f77f36f9fb13fa59
bbritva <amgboris@ya.ru>
2021-10-04 08:38:38 +0300

    накидал функций

--- ex00/Converter.cpp
+++ ex00/Converter.cpp
@@ -16,3 +16,55 @@ void Converter::justDoIt(std::string str)
 	
 
 }
+
+bool Converter::getValue(std::string str, int &res) {
+    try {
+        res = std::stoi(str);
+        return true;
+    }
+    catch(std::invalid_argument e) {
+        return false;
+    }
+    catch(std::out_of_range e) {
+        return false;
+    }
+}
+
+bool Converter::getValue(std::string str, char &res) {
+    // try {
+    //     // res = std::stoi(str);
+    //     // return true;
+    // }
+    // catch(std::invalid_argument e) {
+    //     // return false;
+    // }
+    // catch(std::out_of_range e) {
+        return false;
+    }
+}
+
+bool Converter::getValue(std::string str, float &res) {
+    try {
+        res = std::stof(str);
+        return true;
+    }
+    catch(std::invalid_argument e) {
+        return false;
+    }
+    catch(std::out_of_range e) {
+        return false;
+    }
+}
+
+bool Converter::getValue(std::string str, double &res) {
+    try {
+        res = std::stod(str);
+        return true;
+    }
+    catch(std::invalid_argument e) {
+        return false;
+    }
+    catch(std::out_of_range e) {
+        return false;
+    }
+}
--- ex00/Converter.hpp
+++ ex00/Converter.hpp
@@ -6,11 +6,28 @@
 
 class Converter
 {
+private:
+	std::string str;
+
+	char	char_value;
+	int		int_value;
+	float	float_value;
+	double	double_value;
+
+
 public:
 	Converter();
 	virtual ~Converter();
 	
 	void justDoIt(std::string str);
+	bool getValue(std::string str, int &res);
+	bool getValue(std::string str, float &res);
+	bool getValue(std::string str, double &res);
+	bool getValue(std::string str, char &res);
+	bool simpleConvert(std::string str, int &res);
+	bool simpleConvert(std::string str, float &res);
+	bool simpleConvert(std::string str, double &res);
+	bool simpleConvert(std::string str, char &res);
 };
 
 
